abe7b80f1aae961ef229e6c9b3baf9d9abb1abc1
ruderude <rude1979@gmail.com>
2022-08-25 23:55:02 +0900

    www

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
 </script>
 
 <template>
